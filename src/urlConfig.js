5f14ed7f86fa7265004a983a7b7fcf2c9fe94e25
Vishnu <vishnu007jr@gmail.com>
2021-02-22 23:52:57 +0530

    addede-comments

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 
-
+//Api url end points
 export const API_URL='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4b09b05c48369b1a6f314700c4ce5266&page=1'
 export const SEARCH_URL=`https://api.themoviedb.org/3/search/movie?api_key=4b09b05c48369b1a6f314700c4ce5266&page=1&query="`
 export const IMG_PATH='https://image.tmdb.org/t/p/w1280'
